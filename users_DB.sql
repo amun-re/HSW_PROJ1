3c012eab19d1d7c13415bb4ca136b1dab7acb7f5
amun-re <torben.kuhnke@web.de>
2017-04-18 10:40:20 +0200

    Kalender PHP DB Schnitstelle

--- users_DB.sql
+++ users_DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 13. Apr 2017 um 11:07
+-- Erstellungszeit: 18. Apr 2017 um 10:39
 -- Server-Version: 10.1.21-MariaDB
 -- PHP-Version: 5.6.30
 
@@ -19,6 +19,34 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `users`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `description` int(11) NOT NULL,
+  `public` tinyint(1) NOT NULL,
+  `date` date NOT NULL,
+  `creator` varchar(50) NOT NULL,
+  `location` int(11) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `bdate` date NOT NULL,
+  `edate` date NOT NULL,
+  `min_age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `events`
+--
+
+INSERT INTO `events` (`id`, `name`, `description`, `public`, `date`, `creator`, `location`, `price`, `bdate`, `edate`, `min_age`) VALUES
+(1, 'test', 4, 0, '2017-04-05', '', 0, '0', '2017-04-20', '2017-04-22', 0),
+(2, 'test2', 33, 1, '2017-04-05', '', 0, '0', '0000-00-00', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -52,6 +80,25 @@ CREATE TABLE `members` (
 INSERT INTO `members` (`id`, `username`, `email`, `password`, `salt`) VALUES
 (0, 'kuhnke', 'sdf@web.de', 'f341ec895e27a6de632f00827fb232cc96b27147ee72cdd8d99a71254bb0bceebfe354740d7d2969e2812f87c9475b8dd4be542ad88e5220dc2aac45bfea811d', 'fec31c5e423918d0ac132a42871fc98d27125a0c38030bdebe8050097848dd1d28b09149038f1b0ea4d89b01d92fb78a32a1d07692a3d477eb2687ce252511ef');
 
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
